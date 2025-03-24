a2cf7e3677210b560a844bfa3868abfc7e75c8fa
litingyes <luz.liting@gmail.com>
2025-03-25 00:30:57 +0800

    feat: list theme

---
+++ scripts/themes.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'node:path'
+import { copySync, writeJSONSync } from 'fs-extra'
+import { themes } from 'tm-themes'
+import pkgInfo from '../package.json'
+
+function main() {
+  copySync(resolve(__dirname, '../node_modules/tm-themes/themes'), resolve(__dirname, '../src/themes'))
+
+  pkgInfo.contributes.themes = themes.map(theme => ({
+    id: theme.name,
+    label: theme.displayName,
+    uiTheme: theme.type === 'dark' ? 'vs-dark' : 'vs',
+    path: `./src/themes/${theme.name}.json`,
+  }))
+
+  writeJSONSync(
+    resolve(__dirname, '../package.json'),
+    pkgInfo,
+    {
+      spaces: 2,
+    },
+  )
+}
+
+main()
