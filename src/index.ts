31875815278d421c9c0b364a21f442a5c6c84aa3
litingyes <luz.liting@gmail.com>
2025-03-26 19:56:11 +0800

    perf: remove extra notification

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { defineExtension } from 'reactive-vscode'
-import { window } from 'vscode'
 
 const { activate, deactivate } = defineExtension(() => {
-  window.showInformationMessage('Hello')
 })
 
 export { activate, deactivate }
